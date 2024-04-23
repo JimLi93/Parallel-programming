3935d6fea0f1028c1d9a66d7a2c5ec58d894ae7f
yenku li <liyenku@gmail.com>
2024-04-24 00:28:13 +0800

    hw2b.cc add time calculation

--- HW2-Mandelbrot-Set/hw2b.cc
+++ HW2-Mandelbrot-Set/hw2b.cc
@@ -18,6 +18,7 @@
 
 int iters, width, height, num_threads;
 double left, right, lower, upper;
+double omp_timer[12][12];
 
 void write_png(const char* filename, int iters, int width, int height, const int* buffer) {
     FILE* fp = fopen(filename, "wb");
@@ -50,7 +51,6 @@ void write_png(const char* filename, int iters, int width, int height, const int
                 }
             }
         }
-        
         png_write_row(png_ptr, row);
     }
     free(row);
@@ -64,6 +64,13 @@ int main(int argc, char** argv) {
     cpu_set_t cpu_set;
     sched_getaffinity(0, sizeof(cpu_set), &cpu_set);
     //printf("%d cpus available\n", CPU_COUNT(&cpu_set));
+    int num_threads = CPU_COUNT(&cpu_set);
+    double t1,t2,t3,t4,t5,t6,t7,t8, t9, t10;
+    double full_time = 0;
+    double gather_time = 0;
+    double omp_time = 0;
+    double write_time = 0;
+    double arrange_time = 0;
 
     /* argument parsing */
     assert(argc == 9);
@@ -78,12 +85,13 @@ int main(int argc, char** argv) {
 
     int rank,size;
     MPI_Init(&argc, &argv);
+    t1 = MPI_Wtime();
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int q = height / size; 
     int r = height % size;
-    
+    int q = height / size; 
+
     int row_total = 0;
     if(rank < r){
         row_total = q + 1;
@@ -96,10 +104,13 @@ int main(int argc, char** argv) {
  
     __m128d v_two = _mm_set_pd1(2);
     __m128d v_four = _mm_set_pd1(4);
+    t2 = MPI_Wtime();
 	#pragma omp parallel 
     {
         #pragma omp for schedule(dynamic) 
 	for(int j=0;j<row_total;j++){
+        double omp_t1 = omp_get_wtime();
+
 		int work_on_row = j*size+rank;
 		double y0 = work_on_row * ((upper - lower) / height) + lower;
         __m128d v_y0 = _mm_load_pd1(&y0);
@@ -161,9 +172,14 @@ int main(int argc, char** argv) {
             local_image[j* width + i] = repeats;
             }
         }
-		
+		omp_timer[rank][omp_get_thread_num()] += omp_get_wtime() - omp_t1;
 	}
 	}
+    t3 = MPI_Wtime();
+    omp_time = t3-t2;
+    for(int i=0; i<num_threads; ++i)
+            printf("[Rank=%d][thread_id=%d] %lf\n", rank, i, omp_timer[rank][i]);
+
     int* misordered_image = (int*)malloc(width * height * sizeof(int));
     int recvcounts[size];
     int displs[size];
@@ -182,12 +198,14 @@ int main(int argc, char** argv) {
             displs[i] = displs[i-1] + recvcounts[i-1];
         }
     }
-    
+    t4 = MPI_Wtime();
     MPI_Gatherv(local_image, width * row_total, MPI_INT, misordered_image, recvcounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
-
-
+    t5 = MPI_Wtime();
+    gather_time += (t5 - t4);
+    
     if(rank == 0) {
         int* ordered_image = (int*)malloc(width * height * sizeof(int));
+        t9 = MPI_Wtime();
         #pragma omp parallel
         {
             #pragma omp for schedule(dynamic)
@@ -199,11 +217,22 @@ int main(int argc, char** argv) {
                 }
             }
         }
+        arrange_time = MPI_Wtime()-t9;
+        t6 = MPI_Wtime();
         write_png(filename, iters, width, height, ordered_image);
+        t7 = MPI_Wtime();
+        write_time += t7-t6;
     }
 
     /* draw and cleanup */
     free(local_image);
+    t8 = MPI_Wtime();
+    full_time = t8 - t1;
+    printf("Process %d: (Full time)    %lf s\n", rank, full_time);
+    printf("Process %d: (OMP time)     %lf s\n", rank, omp_time);
+    printf("Process %d: (Gather time)  %lf s\n", rank, gather_time);
+    printf("Process %d: (arrange time) %lf s\n", rank, arrange_time);
+    printf("Process %d: (write time)   %lf s\n", rank, write_time);
     MPI_Finalize();
 
 }
