aa756e2f70e21c51bd3aff6148f52bcb0a19fdfc
yenku li <liyenku@gmail.com>
2024-04-24 00:44:05 +0800

    Finish HW3

---
+++ HW3-All-Pairs-Shortest-Path/hw3-1.cc
@@ -0,0 +1,134 @@
+//Success
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <omp.h>
+
+const int INF = ((1 << 30) - 1);
+const int V = 10000;
+void input(char* inFileName);
+void output(char* outFileName);
+
+void block_FW(int B);
+int ceil(int a, int b);
+void cal(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height);
+
+int n, m;
+static int Dist[V][V];
+int num_of_threads;
+
+int main(int argc, char* argv[]) {
+    cpu_set_t cpu_set;
+    sched_getaffinity(0, sizeof(cpu_set), &cpu_set);
+    //printf("%d cpus available\n", CPU_COUNT(&cpu_set));
+    num_of_threads = CPU_COUNT(&cpu_set);
+    omp_set_num_threads(num_of_threads);
+    input(argv[1]);
+    int B = 16;
+    block_FW(B);
+    output(argv[2]);
+    return 0;
+}
+
+void input(char* infile) {
+    FILE* file = fopen(infile, "rb");
+    fread(&n, sizeof(int), 1, file);
+    fread(&m, sizeof(int), 1, file);
+
+    //int chunksize = n/num_of_threads + 1;
+
+    #pragma omp parallel for simd collapse(2)
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            Dist[i][j] = INF;
+        }
+    }
+    #pragma omp simd
+    for (int i = 0; i < n; ++i) {
+            Dist[i][i] = 0;
+    }
+
+    int pair[3];
+    for (int i = 0; i < m; ++i) {
+        fread(pair, sizeof(int), 3, file);
+        Dist[pair[0]][pair[1]] = pair[2];
+    }
+    fclose(file);
+}
+
+void output(char* outFileName) {
+    FILE* outfile = fopen(outFileName, "w");
+    #pragma omp parallel for simd collapse(2)
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            Dist[i][j] = INF * (Dist[i][j] >= INF) + Dist[i][j] * (Dist[i][j] < INF);
+            //if (Dist[i][j] >= INF) Dist[i][j] = INF;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        fwrite(Dist[i], sizeof(int), n, outfile);
+    }
+    fclose(outfile);
+}
+
+int ceil(int a, int b) { return (a + b - 1) / b; }
+
+void block_FW(int B) {
+    int round = ceil(n, B);
+    for (int r = 0; r < round; ++r) {
+        printf("%d %d\n", r, round);
+        fflush(stdout);
+        /* Phase 1*/
+        cal(B, r, r, r, 1, 1);
+
+        /* Phase 2*/
+        cal(B, r, r, 0, r, 1);
+        cal(B, r, r, r + 1, round - r - 1, 1);
+        cal(B, r, 0, r, 1, r);
+        cal(B, r, r + 1, r, 1, round - r - 1);
+
+        /* Phase 3*/
+        cal(B, r, 0, 0, r, r);
+        cal(B, r, 0, r + 1, round - r - 1, r);
+        cal(B, r, r + 1, 0, r, round - r - 1);
+        cal(B, r, r + 1, r + 1, round - r - 1, round - r - 1);
+    }
+}
+
+void cal(
+    int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height) {
+    int block_end_x = block_start_x + block_height;
+    int block_end_y = block_start_y + block_width;
+
+    int RoundB = Round * B;
+    int RoundB1 = RoundB + B;
+
+    #pragma omp parallel for schedule(dynamic)
+    for (int b_i = block_start_x; b_i < block_end_x; ++b_i) {
+
+        int block_internal_start_x = b_i * B;
+        int block_internal_end_x = (b_i + 1) * B;
+        if (block_internal_end_x > n) block_internal_end_x = n;
+        
+        for (int b_j = block_start_y; b_j < block_end_y; ++b_j) {
+            // To calculate B*B elements in the block (b_i, b_j)
+            // For each block, it need to compute B times
+            int block_internal_start_y = b_j * B;
+            int block_internal_end_y = (b_j + 1) * B;
+            if (block_internal_end_y > n) block_internal_end_y = n;
+            
+            for (int k = RoundB; k < RoundB1 && k < n; ++k) {
+                // To calculate original index of elements in the block (b_i, b_j)
+                // For instance, original index of (0,0) in block (1,2) is (2,5) for V=6,B=2
+
+                for (int i = block_internal_start_x; i < block_internal_end_x; ++i) {
+                    #pragma omp simd
+                    for (int j = block_internal_start_y; j < block_internal_end_y; ++j) {
+                        int tmp = Dist[i][k] + Dist[k][j];
+                        Dist[i][j] = tmp * (tmp < Dist[i][j]) + Dist[i][j] * (tmp >= Dist[i][j]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HW3-All-Pairs-Shortest-Path/hw3-3.cu
@@ -0,0 +1,305 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <omp.h>
+#include <cuda.h>
+
+#define BLOCKFACTOR 64
+#define HALF_FACTOR 32
+#define GPU_NUM 2
+
+const int INF = ((1 << 30) - 1);
+
+int *Dist;
+int *Dev_Dist[GPU_NUM];
+int old_n;
+int n, m;
+
+void input(char* infile) {
+    FILE* file = fopen(infile, "rb");
+    fread(&n, sizeof(int), 1, file);
+    fread(&m, sizeof(int), 1, file);
+    printf("Vertex Num: %d\n", n);
+    printf("Edge   Num: %d\n", m);
+    /* Record the original n (matrix size) */
+    old_n = n;
+
+    /* Padding n to muliple of (BLOCKFACTOR) */
+    int n_r = n % BLOCKFACTOR;
+    if(n_r != 0) n = n - n_r + BLOCKFACTOR;
+
+    /* Memory allocation */
+    cudaMallocHost((void**)&Dist, sizeof(int)*n*n);
+
+    /* Initialize the matrix */
+    #pragma omp parallel for simd collapse(2)
+    for (int i = 0; i < n; ++i) {
+        //int IN = i * n;
+        for (int j = 0; j < n; ++j) {
+            Dist[i * n +j] = INF;
+        }
+    }
+
+    #pragma omp simd
+    for (int i = 0; i < n; ++i) {
+            Dist[i * n + i] = 0;
+    }
+
+    /* Read the input */
+    int * buffer = (int*) malloc(m*3*sizeof(int));
+    fread(buffer, sizeof(int), m*3, file);
+    #pragma omp simd
+    for (int i = 0; i < m; ++i) {
+        //fread(pair, sizeof(int), 3, file);
+        Dist[buffer[i*3] * n + buffer[i*3+1]] = buffer[i*3+2];
+    }
+    fclose(file);
+}
+
+void output(char* outFileName) {
+    FILE *outfile = fopen(outFileName, "w");
+    for (int i = 0; i < old_n; ++i) {
+        fwrite(&Dist[i * n], sizeof(int), old_n, outfile);
+    }
+    fclose(outfile);
+}
+
+int ceil(int a, int b) { return (a + b - 1) / b; }
+
+
+/*  phase 1 of algorithm (GPU kernel) */
+__global__ void phase1(int *global_Dist, int round, int matrixSize) {
+    __shared__ int share_Dist[BLOCKFACTOR][BLOCKFACTOR];
+    int j = threadIdx.x;
+    int i = threadIdx.y;
+    int RB = round * BLOCKFACTOR;
+
+    int global_idx1 = (i + RB              ) * matrixSize + (j + RB              );
+    int global_idx2 = (i + RB              ) * matrixSize + (j + RB + HALF_FACTOR);
+    int global_idx3 = (i + RB + HALF_FACTOR) * matrixSize + (j + RB              );
+    int global_idx4 = (i + RB + HALF_FACTOR) * matrixSize + (j + RB + HALF_FACTOR);
+
+    share_Dist[i              ][j              ] = global_Dist[global_idx1];
+    share_Dist[i              ][j + HALF_FACTOR] = global_Dist[global_idx2];
+    share_Dist[i + HALF_FACTOR][j              ] = global_Dist[global_idx3];
+    share_Dist[i + HALF_FACTOR][j + HALF_FACTOR] = global_Dist[global_idx4];
+
+    #pragma unroll
+    for (int k = 0; k < BLOCKFACTOR; k++) {
+        __syncthreads();
+        share_Dist[i              ][j              ] = min(share_Dist[i              ][k] + share_Dist[k][j              ], share_Dist[i              ][j              ] );
+        share_Dist[i              ][j + HALF_FACTOR] = min(share_Dist[i              ][k] + share_Dist[k][j + HALF_FACTOR], share_Dist[i              ][j + HALF_FACTOR] );
+        share_Dist[i + HALF_FACTOR][j              ] = min(share_Dist[i + HALF_FACTOR][k] + share_Dist[k][j              ], share_Dist[i + HALF_FACTOR][j              ] );
+        share_Dist[i + HALF_FACTOR][j + HALF_FACTOR] = min(share_Dist[i + HALF_FACTOR][k] + share_Dist[k][j + HALF_FACTOR], share_Dist[i + HALF_FACTOR][j + HALF_FACTOR] ); 
+    }
+
+    global_Dist[global_idx1] = share_Dist[i              ][j              ];
+    global_Dist[global_idx2] = share_Dist[i              ][j + HALF_FACTOR];
+    global_Dist[global_idx3] = share_Dist[i + HALF_FACTOR][j              ];
+    global_Dist[global_idx4] = share_Dist[i + HALF_FACTOR][j + HALF_FACTOR];
+}
+
+/*  phase 2 of algorithm (GPU kernel) */
+__global__ void phase2(int *global_Dist, int round, int matrixSize){
+    __shared__ int share_Dist[BLOCKFACTOR][BLOCKFACTOR * 2];
+    int local_round = round;
+    int i = threadIdx.y;
+    int j = threadIdx.x;
+    int blockId = blockIdx.y;
+    int row_col = blockIdx.x;
+
+    int RB = local_round * BLOCKFACTOR;
+
+    int j1 = j;
+    int j2 = j;
+    int global_idx1, global_idx2, global_idx3, global_idx4;
+
+    if(row_col == 0){
+        global_idx1 = (RB + i              ) * matrixSize + ((blockId) * BLOCKFACTOR + j              );
+        global_idx2 = (RB + i              ) * matrixSize + ((blockId) * BLOCKFACTOR + j + HALF_FACTOR);
+        global_idx3 = (RB + i + HALF_FACTOR) * matrixSize + ((blockId) * BLOCKFACTOR + j              );
+        global_idx4 = (RB + i + HALF_FACTOR) * matrixSize + ((blockId) * BLOCKFACTOR + j + HALF_FACTOR);
+        j2 += BLOCKFACTOR;
+    }
+    else {
+        global_idx1 = ((blockId) * BLOCKFACTOR + i              ) * matrixSize + (RB + j              );
+        global_idx2 = ((blockId) * BLOCKFACTOR + i              ) * matrixSize + (RB + j + HALF_FACTOR);
+        global_idx3 = ((blockId) * BLOCKFACTOR + i + HALF_FACTOR) * matrixSize + (RB + j              );
+        global_idx4 = ((blockId) * BLOCKFACTOR + i + HALF_FACTOR) * matrixSize + (RB + j + HALF_FACTOR);
+        j1 += BLOCKFACTOR;
+    }
+    if (blockId == local_round) 
+        return;
+    int global_pivot_idx1 = (RB + i              ) * matrixSize + RB + j              ;
+    int global_pivot_idx2 = (RB + i              ) * matrixSize + RB + j + HALF_FACTOR;
+    int global_pivot_idx3 = (RB + i + HALF_FACTOR) * matrixSize + RB + j              ;
+    int global_pivot_idx4 = (RB + i + HALF_FACTOR) * matrixSize + RB + j + HALF_FACTOR;
+
+    share_Dist[i              ][j1              ] = global_Dist[global_idx1];
+    share_Dist[i              ][j1 + HALF_FACTOR] = global_Dist[global_idx2];
+    share_Dist[i + HALF_FACTOR][j1              ] = global_Dist[global_idx3];
+    share_Dist[i + HALF_FACTOR][j1 + HALF_FACTOR] = global_Dist[global_idx4];
+
+    share_Dist[i              ][j2              ] = global_Dist[global_pivot_idx1];
+    share_Dist[i              ][j2 + HALF_FACTOR] = global_Dist[global_pivot_idx2];
+    share_Dist[i + HALF_FACTOR][j2              ] = global_Dist[global_pivot_idx3];
+    share_Dist[i + HALF_FACTOR][j2 + HALF_FACTOR] = global_Dist[global_pivot_idx4];
+  
+    __syncthreads();
+
+    #pragma unroll
+    for (int k = 0; k < BLOCKFACTOR; k++) {
+        //__syncthreads();
+        share_Dist[i              ][j1              ] = min(share_Dist[i              ][j1              ], share_Dist[i              ][k + BLOCKFACTOR] + share_Dist[k][j              ]);
+        share_Dist[i              ][j1 + HALF_FACTOR] = min(share_Dist[i              ][j1 + HALF_FACTOR], share_Dist[i              ][k + BLOCKFACTOR] + share_Dist[k][j+HALF_FACTOR]);
+        share_Dist[i + HALF_FACTOR][j1              ] = min(share_Dist[i + HALF_FACTOR][j1              ], share_Dist[i + HALF_FACTOR][k + BLOCKFACTOR] + share_Dist[k][j              ]);
+        share_Dist[i + HALF_FACTOR][j1 + HALF_FACTOR] = min(share_Dist[i + HALF_FACTOR][j1 + HALF_FACTOR], share_Dist[i + HALF_FACTOR][k + BLOCKFACTOR] + share_Dist[k][j+HALF_FACTOR]);
+
+    }
+global_Dist[global_idx1] = share_Dist[i              ][j1              ];
+global_Dist[global_idx2] = share_Dist[i              ][j1 + HALF_FACTOR];
+global_Dist[global_idx3] = share_Dist[i + HALF_FACTOR][j1              ];
+global_Dist[global_idx4] = share_Dist[i + HALF_FACTOR][j1 + HALF_FACTOR];
+}
+
+
+/*  phase 3 of algorithm (GPU kernel) */
+__global__ void phase3(int *global_Dist, int round, int matrixSize, int start){
+    __shared__ int share_Dist[BLOCKFACTOR][BLOCKFACTOR * 2];
+    int local_round = round;
+    int j = threadIdx.x;
+    int i = threadIdx.y;
+    int block_j = blockIdx.x;
+    int block_i = blockIdx.y + start;
+    int global_pivot_i = (block_i) * BLOCKFACTOR + i;
+    int global_pivot_j = (block_j) * BLOCKFACTOR + j;
+
+    int RB = local_round * BLOCKFACTOR;
+    if(block_i == round || block_j == round){
+        return ;
+    }
+
+    int global_idx1 = (global_pivot_i              ) * matrixSize + global_pivot_j              ;
+    int global_idx2 = (global_pivot_i              ) * matrixSize + global_pivot_j + HALF_FACTOR;
+    int global_idx3 = (global_pivot_i + HALF_FACTOR) * matrixSize + global_pivot_j              ;
+    int global_idx4 = (global_pivot_i + HALF_FACTOR) * matrixSize + global_pivot_j + HALF_FACTOR;
+    
+    int global_pivot_row_idx1 = (global_pivot_i              ) * matrixSize + RB + j              ;
+    int global_pivot_row_idx2 = (global_pivot_i              ) * matrixSize + RB + j + HALF_FACTOR;
+    int global_pivot_row_idx3 = (global_pivot_i + HALF_FACTOR) * matrixSize + RB + j              ;
+    int global_pivot_row_idx4 = (global_pivot_i + HALF_FACTOR) * matrixSize + RB + j + HALF_FACTOR;
+
+    int global_pivot_col_idx1 = (RB + i              ) * matrixSize + global_pivot_j              ;
+    int global_pivot_col_idx2 = (RB + i              ) * matrixSize + global_pivot_j + HALF_FACTOR;
+    int global_pivot_col_idx3 = (RB + i + HALF_FACTOR) * matrixSize + global_pivot_j              ;
+    int global_pivot_col_idx4 = (RB + i + HALF_FACTOR) * matrixSize + global_pivot_j + HALF_FACTOR;
+
+    int share_tmp1 = global_Dist[global_idx1];
+    int share_tmp2 = global_Dist[global_idx2];
+    int share_tmp3 = global_Dist[global_idx3];
+    int share_tmp4 = global_Dist[global_idx4];
+
+    share_Dist[i              ][j              ] = global_Dist[global_pivot_row_idx1];
+    share_Dist[i              ][j + HALF_FACTOR] = global_Dist[global_pivot_row_idx2];
+    share_Dist[i + HALF_FACTOR][j              ] = global_Dist[global_pivot_row_idx3];
+    share_Dist[i + HALF_FACTOR][j + HALF_FACTOR] = global_Dist[global_pivot_row_idx4];
+
+    share_Dist[i              ][j + BLOCKFACTOR              ] = global_Dist[global_pivot_col_idx1];
+    share_Dist[i              ][j + BLOCKFACTOR + HALF_FACTOR] = global_Dist[global_pivot_col_idx2];
+    share_Dist[i + HALF_FACTOR][j + BLOCKFACTOR              ] = global_Dist[global_pivot_col_idx3];
+    share_Dist[i + HALF_FACTOR][j + BLOCKFACTOR + HALF_FACTOR] = global_Dist[global_pivot_col_idx4];
+
+    __syncthreads();
+
+    #pragma unroll
+    for(int k=0;k<BLOCKFACTOR;k++){
+        //__syncthreads();
+        share_tmp1 = min(share_Dist[i              ][k] + share_Dist[k][j + BLOCKFACTOR              ], share_tmp1); 
+        share_tmp2 = min(share_Dist[i              ][k] + share_Dist[k][j + BLOCKFACTOR + HALF_FACTOR], share_tmp2); 
+        share_tmp3 = min(share_Dist[i + HALF_FACTOR][k] + share_Dist[k][j + BLOCKFACTOR              ], share_tmp3); 
+        share_tmp4 = min(share_Dist[i + HALF_FACTOR][k] + share_Dist[k][j + BLOCKFACTOR + HALF_FACTOR], share_tmp4);     
+    }
+
+    global_Dist[global_idx1] = share_tmp1;
+    global_Dist[global_idx2] = share_tmp2;
+    global_Dist[global_idx3] = share_tmp3;
+    global_Dist[global_idx4] = share_tmp4;
+}
+
+
+void block_FW() {
+    int round = ceil(n, BLOCKFACTOR);
+
+    cudaHostRegister(Dist, n*n*sizeof(int), cudaHostRegisterDefault);
+
+
+
+	#pragma omp parallel num_threads(GPU_NUM)
+	{
+        int gpu_id = omp_get_thread_num();
+		cudaSetDevice(gpu_id);
+        cudaMalloc(&Dev_Dist[gpu_id], n * n * sizeof(int));
+
+        int start_x, end_x;
+        
+        if(gpu_id == 0){
+            start_x = 0;
+            end_x = round / 2;
+        }
+        else if(gpu_id == 1) {
+            start_x = round / 2;
+            end_x = round;
+        }
+	
+        dim3 block(HALF_FACTOR,HALF_FACTOR);
+        dim3 grid1(1);
+        dim3 grid2(2, round);
+        dim3 grid3(round, end_x-start_x);
+
+        
+        cudaMemcpy(Dev_Dist[gpu_id] + start_x * BLOCKFACTOR * n, Dist + start_x * BLOCKFACTOR * n, BLOCKFACTOR * n * (end_x - start_x) * sizeof(int), cudaMemcpyHostToDevice);
+
+
+
+        for(int r=0;r<round/2;r++){
+            int target_id = 0;
+            if(gpu_id == target_id){
+                cudaMemcpy(Dist + r * BLOCKFACTOR * n, Dev_Dist[gpu_id] + r * BLOCKFACTOR * n, BLOCKFACTOR * n * sizeof(int), cudaMemcpyDeviceToHost);
+            }
+            #pragma omp barrier
+            if(gpu_id != target_id){
+                cudaMemcpy(Dev_Dist[gpu_id] + r * BLOCKFACTOR * n, Dist + r * BLOCKFACTOR * n, BLOCKFACTOR * n * sizeof(int), cudaMemcpyHostToDevice);
+            }
+            phase1 <<<grid1, block>>>(Dev_Dist[gpu_id], r, n);
+            phase2 <<<grid2, block>>>(Dev_Dist[gpu_id], r, n);
+            phase3 <<<grid3, block>>>(Dev_Dist[gpu_id], r, n, start_x);
+        }
+
+        for(int r=round/2;r<round;r++){
+            int target_id = 1;
+            if(gpu_id == target_id){
+                cudaMemcpy(Dist + r * BLOCKFACTOR * n, Dev_Dist[gpu_id] + r * BLOCKFACTOR * n, BLOCKFACTOR * n * sizeof(int), cudaMemcpyDeviceToHost);
+            }
+            #pragma omp barrier
+            if(gpu_id != target_id){
+                cudaMemcpy(Dev_Dist[gpu_id] + r * BLOCKFACTOR * n, Dist + r * BLOCKFACTOR * n, BLOCKFACTOR * n * sizeof(int), cudaMemcpyHostToDevice);
+            }
+            phase1 <<<grid1, block>>>(Dev_Dist[gpu_id], r, n);
+            phase2 <<<grid2, block>>>(Dev_Dist[gpu_id], r, n);
+            phase3 <<<grid3, block>>>(Dev_Dist[gpu_id], r, n, start_x);
+        }
+
+		cudaMemcpy(Dist + start_x * BLOCKFACTOR * n, Dev_Dist[gpu_id] + start_x * BLOCKFACTOR * n, BLOCKFACTOR * n * (end_x - start_x) * sizeof(int), cudaMemcpyDeviceToHost);
+    }
+    cudaFree(Dev_Dist);
+}
+
+int main(int argc, char *argv[]){
+
+    input(argv[1]);
+	block_FW();
+	output(argv[2]);
+    cudaFreeHost(Dist);
+	return 0;
+
+}
