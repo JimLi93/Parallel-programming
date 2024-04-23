1270ee40b3287e93be54b1b1c1ced9934a54d105
yenku li <liyenku@gmail.com>
2024-04-24 00:53:44 +0800

    hw3-2.cu add time calculation

--- HW3-All-Pairs-Shortest-Path/hw3-2.cu
+++ HW3-All-Pairs-Shortest-Path/hw3-2.cu
@@ -1,8 +1,14 @@
+// Combine phase 2-1 and phase 2-2
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda.h>
 #include <iostream>
 #include <omp.h>
+#include <chrono>
+
+//======================
+#define DEV_NO 0
+cudaDeviceProp prop;
 
 #define BLOCKFACTOR 64
 #define HALF_FACTOR 32
@@ -12,6 +18,21 @@ int *Dev_Dist;
 int old_n;
 int n, m;
 
+double cal_time(struct timespec start, struct timespec end)
+{
+	struct timespec temp;
+	if ((end.tv_nsec - start.tv_nsec) < 0)
+	{
+		temp.tv_sec = end.tv_sec - start.tv_sec - 1;
+		temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+	}
+	else
+	{
+		temp.tv_sec = end.tv_sec - start.tv_sec;
+		temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+	}
+	return temp.tv_sec + (double)temp.tv_nsec / 1000000000.0;
+}
 
 void input(char* infile) {
     FILE* file = fopen(infile, "rb");
@@ -64,6 +85,8 @@ void output(char* outFileName) {
 
 int ceil(int a, int b) { return (a + b - 1) / b; }
 
+/*  matrixSize, BLOCKFACTOR, totalRound  (memory in GPU) */
+//__constant__ int info[3]; 
 
 /*  phase 1 of algorithm (GPU kernel) */
 __global__ void phase1(int *global_Dist, int round, int matrixSize) {
@@ -71,35 +94,36 @@ __global__ void phase1(int *global_Dist, int round, int matrixSize) {
     int j = threadIdx.x;
     int i = threadIdx.y;
     int RB = round * BLOCKFACTOR;
+    //int BB = BLOCKFACTOR * BLOCKFACTOR;
 
-    int global_idx1 = (i + RB              ) * matrixSize + (j + RB              );
-    int global_idx2 = (i + RB              ) * matrixSize + (j + RB + HALF_FACTOR);
-    int global_idx3 = (i + RB + HALF_FACTOR) * matrixSize + (j + RB              );
+    int global_idx1 = (i + RB) * matrixSize + (j + RB);
+    int global_idx2 = (i + RB) * matrixSize + (j + RB + HALF_FACTOR);
+    int global_idx3 = (i + RB + HALF_FACTOR) * matrixSize + (j + RB);
     int global_idx4 = (i + RB + HALF_FACTOR) * matrixSize + (j + RB + HALF_FACTOR);
 
-    share_Dist[i              ][j              ] = global_Dist[global_idx1];
-    share_Dist[i              ][j + HALF_FACTOR] = global_Dist[global_idx2];
-    share_Dist[i + HALF_FACTOR][j              ] = global_Dist[global_idx3];
+    share_Dist[i     ][j     ] = global_Dist[global_idx1];
+    share_Dist[i     ][j + HALF_FACTOR] = global_Dist[global_idx2];
+    share_Dist[i + HALF_FACTOR][j     ] = global_Dist[global_idx3];
     share_Dist[i + HALF_FACTOR][j + HALF_FACTOR] = global_Dist[global_idx4];
 
     #pragma unroll
     for (int k = 0; k < BLOCKFACTOR; k++) {
         __syncthreads();
-        share_Dist[i              ][j              ] = min(share_Dist[i              ][k] + share_Dist[k][j              ], share_Dist[i              ][j              ] );
-        share_Dist[i              ][j + HALF_FACTOR] = min(share_Dist[i              ][k] + share_Dist[k][j + HALF_FACTOR], share_Dist[i              ][j + HALF_FACTOR] );
-        share_Dist[i + HALF_FACTOR][j              ] = min(share_Dist[i + HALF_FACTOR][k] + share_Dist[k][j              ], share_Dist[i + HALF_FACTOR][j              ] );
+        share_Dist[i     ][j     ] = min(share_Dist[i     ][k] + share_Dist[k][j     ], share_Dist[i     ][j     ] );
+        share_Dist[i     ][j + HALF_FACTOR] = min(share_Dist[i     ][k] + share_Dist[k][j + HALF_FACTOR], share_Dist[i     ][j + HALF_FACTOR] );
+        share_Dist[i + HALF_FACTOR][j     ] = min(share_Dist[i + HALF_FACTOR][k] + share_Dist[k][j     ], share_Dist[i + HALF_FACTOR][j     ] );
         share_Dist[i + HALF_FACTOR][j + HALF_FACTOR] = min(share_Dist[i + HALF_FACTOR][k] + share_Dist[k][j + HALF_FACTOR], share_Dist[i + HALF_FACTOR][j + HALF_FACTOR] ); 
     }
 
-    global_Dist[global_idx1] = share_Dist[i              ][j              ];
-    global_Dist[global_idx2] = share_Dist[i              ][j + HALF_FACTOR];
-    global_Dist[global_idx3] = share_Dist[i + HALF_FACTOR][j              ];
+    global_Dist[global_idx1] = share_Dist[i     ][j     ];
+    global_Dist[global_idx2] = share_Dist[i     ][j + HALF_FACTOR];
+    global_Dist[global_idx3] = share_Dist[i + HALF_FACTOR][j     ];
     global_Dist[global_idx4] = share_Dist[i + HALF_FACTOR][j + HALF_FACTOR];
 }
 
-/*  phase 2 of algorithm (GPU kernel) */
-__global__ void phase2(int *global_Dist, int round, int matrixSize){
-    __shared__ int share_Dist[BLOCKFACTOR][BLOCKFACTOR * 2];
+/*  phase 2-1 of algorithm (GPU kernel) */
+__global__ void phase2_1(int *global_Dist, int round, int matrixSize){
+    __shared__ int share_Dist[BLOCKFACTOR][BLOCKFACTOR*2];
     int local_round = round;
     int i = threadIdx.y;
     int j = threadIdx.x;
@@ -113,51 +137,51 @@ __global__ void phase2(int *global_Dist, int round, int matrixSize){
     int global_idx1, global_idx2, global_idx3, global_idx4;
 
     if(row_col == 0){
-        global_idx1 = (RB + i              ) * matrixSize + ((blockId) * BLOCKFACTOR + j              );
-        global_idx2 = (RB + i              ) * matrixSize + ((blockId) * BLOCKFACTOR + j + HALF_FACTOR);
-        global_idx3 = (RB + i + HALF_FACTOR) * matrixSize + ((blockId) * BLOCKFACTOR + j              );
+        global_idx1 = (RB + i) * matrixSize + ((blockId) * BLOCKFACTOR + j);
+        global_idx2 = (RB + i) * matrixSize + ((blockId) * BLOCKFACTOR + j + HALF_FACTOR);
+        global_idx3 = (RB + i + HALF_FACTOR) * matrixSize + ((blockId) * BLOCKFACTOR + j);
         global_idx4 = (RB + i + HALF_FACTOR) * matrixSize + ((blockId) * BLOCKFACTOR + j + HALF_FACTOR);
         j2 += BLOCKFACTOR;
     }
     else {
-        global_idx1 = ((blockId) * BLOCKFACTOR + i              ) * matrixSize + (RB + j              );
-        global_idx2 = ((blockId) * BLOCKFACTOR + i              ) * matrixSize + (RB + j + HALF_FACTOR);
-        global_idx3 = ((blockId) * BLOCKFACTOR + i + HALF_FACTOR) * matrixSize + (RB + j              );
+        global_idx1 = ((blockId) * BLOCKFACTOR + i) * matrixSize + (RB + j);
+        global_idx2 = ((blockId) * BLOCKFACTOR + i) * matrixSize + (RB + j + HALF_FACTOR);
+        global_idx3 = ((blockId) * BLOCKFACTOR + i + HALF_FACTOR) * matrixSize + (RB + j);
         global_idx4 = ((blockId) * BLOCKFACTOR + i + HALF_FACTOR) * matrixSize + (RB + j + HALF_FACTOR);
         j1 += BLOCKFACTOR;
     }
     if (blockId == local_round) 
         return;
-    int global_pivot_idx1 = (RB + i              ) * matrixSize + RB + j              ;
-    int global_pivot_idx2 = (RB + i              ) * matrixSize + RB + j + HALF_FACTOR;
-    int global_pivot_idx3 = (RB + i + HALF_FACTOR) * matrixSize + RB + j              ;
+    int global_pivot_idx1 = (RB + i) * matrixSize + RB + j;
+    int global_pivot_idx2 = (RB + i) * matrixSize + RB + j + HALF_FACTOR;
+    int global_pivot_idx3 = (RB + i + HALF_FACTOR) * matrixSize + RB + j;
     int global_pivot_idx4 = (RB + i + HALF_FACTOR) * matrixSize + RB + j + HALF_FACTOR;
 
-    share_Dist[i              ][j1              ] = global_Dist[global_idx1];
-    share_Dist[i              ][j1 + HALF_FACTOR] = global_Dist[global_idx2];
-    share_Dist[i + HALF_FACTOR][j1              ] = global_Dist[global_idx3];
-    share_Dist[i + HALF_FACTOR][j1 + HALF_FACTOR] = global_Dist[global_idx4];
+    share_Dist[i   ][j1   ] = global_Dist[global_idx1];
+    share_Dist[i][j1 +HALF_FACTOR  ] = global_Dist[global_idx2];
+    share_Dist[i +HALF_FACTOR  ][j1] = global_Dist[global_idx3];
+    share_Dist[i+HALF_FACTOR][j1+HALF_FACTOR] = global_Dist[global_idx4];
 
-    share_Dist[i              ][j2              ] = global_Dist[global_pivot_idx1];
-    share_Dist[i              ][j2 + HALF_FACTOR] = global_Dist[global_pivot_idx2];
-    share_Dist[i + HALF_FACTOR][j2              ] = global_Dist[global_pivot_idx3];
-    share_Dist[i + HALF_FACTOR][j2 + HALF_FACTOR] = global_Dist[global_pivot_idx4];
+    share_Dist[i   ][j2   ] = global_Dist[global_pivot_idx1];
+    share_Dist[i][j2 +HALF_FACTOR  ] = global_Dist[global_pivot_idx2];
+    share_Dist[i +HALF_FACTOR  ][j2] = global_Dist[global_pivot_idx3];
+    share_Dist[i+HALF_FACTOR][j2+HALF_FACTOR] = global_Dist[global_pivot_idx4];
   
     __syncthreads();
 
     #pragma unroll
     for (int k = 0; k < BLOCKFACTOR; k++) {
         //__syncthreads();
-        share_Dist[i              ][j1              ] = min(share_Dist[i              ][j1              ], share_Dist[i              ][k + BLOCKFACTOR] + share_Dist[k][j              ]);
-        share_Dist[i              ][j1 + HALF_FACTOR] = min(share_Dist[i              ][j1 + HALF_FACTOR], share_Dist[i              ][k + BLOCKFACTOR] + share_Dist[k][j+HALF_FACTOR]);
-        share_Dist[i + HALF_FACTOR][j1              ] = min(share_Dist[i + HALF_FACTOR][j1              ], share_Dist[i + HALF_FACTOR][k + BLOCKFACTOR] + share_Dist[k][j              ]);
-        share_Dist[i + HALF_FACTOR][j1 + HALF_FACTOR] = min(share_Dist[i + HALF_FACTOR][j1 + HALF_FACTOR], share_Dist[i + HALF_FACTOR][k + BLOCKFACTOR] + share_Dist[k][j+HALF_FACTOR]);
+        share_Dist[i   ][j1   ] = min(share_Dist[i][j1], share_Dist[i][k+BLOCKFACTOR] + share_Dist[k][j]);
+        share_Dist[i][j1 +HALF_FACTOR  ] = min(share_Dist[i][j1+HALF_FACTOR], share_Dist[i][k+BLOCKFACTOR] + share_Dist[k][j+HALF_FACTOR]);
+        share_Dist[i+HALF_FACTOR   ][j1] = min(share_Dist[i+HALF_FACTOR][j1], share_Dist[i+HALF_FACTOR][k+BLOCKFACTOR] + share_Dist[k][j]);
+        share_Dist[i+HALF_FACTOR][j1+HALF_FACTOR] = min(share_Dist[i+HALF_FACTOR][j1+HALF_FACTOR], share_Dist[i+HALF_FACTOR][k+BLOCKFACTOR] + share_Dist[k][j+HALF_FACTOR]);
 
     }
-global_Dist[global_idx1] = share_Dist[i              ][j1              ];
-global_Dist[global_idx2] = share_Dist[i              ][j1 + HALF_FACTOR];
-global_Dist[global_idx3] = share_Dist[i + HALF_FACTOR][j1              ];
-global_Dist[global_idx4] = share_Dist[i + HALF_FACTOR][j1 + HALF_FACTOR];
+global_Dist[global_idx1]         = share_Dist[i   ][j1   ];
+global_Dist[global_idx2]    = share_Dist[i][j1 +HALF_FACTOR  ];
+global_Dist[global_idx3]      = share_Dist[i +HALF_FACTOR  ][j1];
+global_Dist[global_idx4] = share_Dist[i+HALF_FACTOR][j1+HALF_FACTOR];
 }
 
 
@@ -173,23 +197,23 @@ __global__ void phase3(int *global_Dist, int round, int matrixSize){
     int global_pivot_j = (block_j) * BLOCKFACTOR + j;
 
     int RB = local_round * BLOCKFACTOR;
-    if(block_i == round || block_j == round){
+    if(block_i == round && block_j == round){
         return ;
     }
 
-    int global_idx1 = (global_pivot_i              ) * matrixSize + global_pivot_j              ;
-    int global_idx2 = (global_pivot_i              ) * matrixSize + global_pivot_j + HALF_FACTOR;
-    int global_idx3 = (global_pivot_i + HALF_FACTOR) * matrixSize + global_pivot_j              ;
+    int global_idx1 = global_pivot_i * matrixSize + global_pivot_j;
+    int global_idx2 = global_pivot_i * matrixSize + global_pivot_j + HALF_FACTOR;
+    int global_idx3 = (global_pivot_i + HALF_FACTOR) * matrixSize + global_pivot_j;
     int global_idx4 = (global_pivot_i + HALF_FACTOR) * matrixSize + global_pivot_j + HALF_FACTOR;
     
-    int global_pivot_row_idx1 = (global_pivot_i              ) * matrixSize + RB + j              ;
-    int global_pivot_row_idx2 = (global_pivot_i              ) * matrixSize + RB + j + HALF_FACTOR;
-    int global_pivot_row_idx3 = (global_pivot_i + HALF_FACTOR) * matrixSize + RB + j              ;
+    int global_pivot_row_idx1 = global_pivot_i * matrixSize + RB + j;
+    int global_pivot_row_idx2 = global_pivot_i * matrixSize + RB + j + HALF_FACTOR;
+    int global_pivot_row_idx3 = (global_pivot_i + HALF_FACTOR) * matrixSize + RB + j;
     int global_pivot_row_idx4 = (global_pivot_i + HALF_FACTOR) * matrixSize + RB + j + HALF_FACTOR;
 
-    int global_pivot_col_idx1 = (RB + i              ) * matrixSize + global_pivot_j              ;
-    int global_pivot_col_idx2 = (RB + i              ) * matrixSize + global_pivot_j + HALF_FACTOR;
-    int global_pivot_col_idx3 = (RB + i + HALF_FACTOR) * matrixSize + global_pivot_j              ;
+    int global_pivot_col_idx1 = (RB + i) * matrixSize + global_pivot_j;
+    int global_pivot_col_idx2 = (RB + i) * matrixSize + global_pivot_j + HALF_FACTOR;
+    int global_pivot_col_idx3 = (RB + i + HALF_FACTOR) * matrixSize + global_pivot_j;
     int global_pivot_col_idx4 = (RB + i + HALF_FACTOR) * matrixSize + global_pivot_j + HALF_FACTOR;
 
     int share_tmp1 = global_Dist[global_idx1];
@@ -197,25 +221,25 @@ __global__ void phase3(int *global_Dist, int round, int matrixSize){
     int share_tmp3 = global_Dist[global_idx3];
     int share_tmp4 = global_Dist[global_idx4];
 
-    share_Dist[i              ][j              ] = global_Dist[global_pivot_row_idx1];
-    share_Dist[i              ][j + HALF_FACTOR] = global_Dist[global_pivot_row_idx2];
-    share_Dist[i + HALF_FACTOR][j              ] = global_Dist[global_pivot_row_idx3];
-    share_Dist[i + HALF_FACTOR][j + HALF_FACTOR] = global_Dist[global_pivot_row_idx4];
+    share_Dist[i][j] = global_Dist[global_pivot_row_idx1];
+    share_Dist[i][j+HALF_FACTOR] = global_Dist[global_pivot_row_idx2];
+    share_Dist[i+HALF_FACTOR][j] = global_Dist[global_pivot_row_idx3];
+    share_Dist[i+HALF_FACTOR][j+HALF_FACTOR] = global_Dist[global_pivot_row_idx4];
 
-    share_Dist[i              ][j + BLOCKFACTOR              ] = global_Dist[global_pivot_col_idx1];
-    share_Dist[i              ][j + BLOCKFACTOR + HALF_FACTOR] = global_Dist[global_pivot_col_idx2];
-    share_Dist[i + HALF_FACTOR][j + BLOCKFACTOR              ] = global_Dist[global_pivot_col_idx3];
-    share_Dist[i + HALF_FACTOR][j + BLOCKFACTOR + HALF_FACTOR] = global_Dist[global_pivot_col_idx4];
+    share_Dist[i][j+BLOCKFACTOR] = global_Dist[global_pivot_col_idx1];
+    share_Dist[i][j+HALF_FACTOR+BLOCKFACTOR] = global_Dist[global_pivot_col_idx2];
+    share_Dist[i+HALF_FACTOR][j+BLOCKFACTOR] = global_Dist[global_pivot_col_idx3];
+    share_Dist[i+HALF_FACTOR][j+HALF_FACTOR+BLOCKFACTOR] = global_Dist[global_pivot_col_idx4];
 
     __syncthreads();
 
     #pragma unroll
     for(int k=0;k<BLOCKFACTOR;k++){
         //__syncthreads();
-        share_tmp1 = min(share_Dist[i              ][k] + share_Dist[k][j + BLOCKFACTOR              ], share_tmp1); 
-        share_tmp2 = min(share_Dist[i              ][k] + share_Dist[k][j + BLOCKFACTOR + HALF_FACTOR], share_tmp2); 
-        share_tmp3 = min(share_Dist[i + HALF_FACTOR][k] + share_Dist[k][j + BLOCKFACTOR              ], share_tmp3); 
-        share_tmp4 = min(share_Dist[i + HALF_FACTOR][k] + share_Dist[k][j + BLOCKFACTOR + HALF_FACTOR], share_tmp4);     
+        share_tmp1 = min(share_Dist[i][k] + share_Dist[k][+ j + BLOCKFACTOR], share_tmp1); 
+        share_tmp2 = min(share_Dist[i][k] + share_Dist[k][j + HALF_FACTOR + BLOCKFACTOR], share_tmp2); 
+        share_tmp3 = min(share_Dist[(i+HALF_FACTOR)][k] + share_Dist[k][j + BLOCKFACTOR], share_tmp3); 
+        share_tmp4 = min(share_Dist[(i+HALF_FACTOR)][k] + share_Dist[k][j + HALF_FACTOR + BLOCKFACTOR], share_tmp4);     
     }
 
     global_Dist[global_idx1] = share_tmp1;
@@ -224,28 +248,66 @@ __global__ void phase3(int *global_Dist, int round, int matrixSize){
     global_Dist[global_idx4] = share_tmp4;
 }
 
-void block_FW(){
-    int round = ceil(n, BLOCKFACTOR);
-    cudaMalloc((void**)&Dev_Dist, sizeof(int) *n * n);
-    cudaMemcpy(Dev_Dist, Dist, sizeof(int) * n * n, cudaMemcpyHostToDevice);
+void block_FW(int B){
+    int round = ceil(n, B);
+    //cudaMalloc((void**)&Dev_Dist, sizeof(int) *n * n);
+    //cudaMemcpy(Dev_Dist, Dist, sizeof(int) * n * n, cudaMemcpyHostToDevice);
     dim3 block(HALF_FACTOR,HALF_FACTOR);
-    dim3 grid1(1);
-    dim3 grid2(2, round);
+    dim3 grid1(2, round);
+    //dim3 grid2(round-1, 1);
     dim3 grid3(round, round);
+    //int tmp_BB = BLOCKFACTOR * BLOCKFACTOR;
     for (int r = 0; r < round; ++r) {  
-        phase1 <<< grid1, block >>>(Dev_Dist, r, n);
-        phase2 <<< grid2, block >>>(Dev_Dist, r, n);
-        phase3 <<< grid3, block >>>(Dev_Dist, r, n);        
+        phase1  <<< 1    ,block>>>(Dev_Dist, r, n);
+        phase2_1<<< grid1, block >>>(Dev_Dist, r, n);
+        //phase2_2<BLOCKFACTOR><<< grid2, block, 2*tmp_BB*sizeof(int) >>>(Dev_Dist, r, n);
+        phase3 <<< grid3, block>>>(Dev_Dist, r, n);        
     }
-    cudaMemcpy(Dist, Dev_Dist, sizeof(int) * n * n, cudaMemcpyDeviceToHost);
+    //cudaDeviceSynchronize();
+    //cudaMemcpy(Dist, Dev_Dist, sizeof(int) * n * n, cudaMemcpyDeviceToHost);
 }
 
 
 int main(int argc, char* argv[]) {
+
+    //double t1,t2,t3,t4;
+	timespec timestamp1, timestamp2, timestamp3, timestamp4, timestamp5, timestamp6, timestamp7;
+    std::chrono::steady_clock::time_point t1;
+    std::chrono::steady_clock::time_point t2;
+    t1 = std::chrono::steady_clock::now();
+    cudaSetDevice(0);
+	clock_gettime(CLOCK_MONOTONIC, &timestamp1);
+	//cudaSetDevice(0);
+
     input(argv[1]);
-    block_FW();
+    clock_gettime(CLOCK_MONOTONIC, &timestamp2);
+    int B = BLOCKFACTOR;
+    //cudaGetDeviceProperties(&prop, DEV_NO);
+    //printf("maxThreasPerBlock = %d, sharedMemPerBlock = %d\n", prop.maxThreadsPerBlock, prop.sharedMemPerBlock);
+    //clock_gettime(CLOCK_MONOTONIC, &timestamp3);
+    cudaMalloc((void**)&Dev_Dist, sizeof(int) *n * n);
+    clock_gettime(CLOCK_MONOTONIC, &timestamp3);
+    cudaMemcpy(Dev_Dist, Dist, sizeof(int) * n * n, cudaMemcpyHostToDevice);
+    //cudaDeviceSynchronize();
+    clock_gettime(CLOCK_MONOTONIC, &timestamp4);
+    block_FW(B);
+    cudaDeviceSynchronize();
+    clock_gettime(CLOCK_MONOTONIC, &timestamp5);
+    cudaMemcpy(Dist, Dev_Dist, sizeof(int) * n * n, cudaMemcpyDeviceToHost);
+    //cudaDeviceSynchronize();
+    clock_gettime(CLOCK_MONOTONIC, &timestamp6);
     output(argv[2]);
+    clock_gettime(CLOCK_MONOTONIC, &timestamp7);
     cudaFreeHost(Dist);
     cudaFree(Dev_Dist);
+    printf("Total  time: %.5f\n", cal_time(timestamp1, timestamp7));
+    printf("Input  time: %.5f\n", cal_time(timestamp1, timestamp2));
+    printf("Malloc time: %.5f\n", cal_time(timestamp2, timestamp3));
+    printf("Memcpy time: %.5f\n", cal_time(timestamp3, timestamp4));
+    printf("GPU    time: %.5f\n", cal_time(timestamp4, timestamp5));
+    printf("Memcpy time: %.5f\n", cal_time(timestamp5, timestamp6));
+    printf("Output time: %.5f\n", cal_time(timestamp6, timestamp7));
+    t2 = std::chrono::steady_clock::now();
+    std::cout << "[Output] " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << "\n";
     return 0;
 }
